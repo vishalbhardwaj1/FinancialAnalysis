732e221e18bc7d8c55a323a4142757bde5624114
VB <vishalbhardwaj1@users.noreply.github.com>
2025-09-12 17:50:10 -0400

    updates
    updates

--- financialtool/yahoofinance/FetchStockData.py
+++ financialtool/yahoofinance/FetchStockData.py
@@ -1,14 +1,18 @@
 import yfinance as yf
 from financialtool.databases.DataBaseUtils import *
+import datetime
 
 data = get_company_ticker_data()
 for ticker in data:
     stock = yf.Ticker(ticker)
     date = get_last_date_for_company(ticker)
-    if date:
+    if date and date != datetime.date.today().strftime('%Y-%m-%d'):
         print(f"Fetching data for {ticker} since {date}...")
         hist = stock.history(start=date, interval="1d")
         hist = hist[1:]
+    elif date == datetime.date.today().strftime('%Y-%m-%d'):
+        print(f"Data for {ticker} is already up to date.")
+        continue
     else:
         hist = stock.history(period="max")
     batch = []
